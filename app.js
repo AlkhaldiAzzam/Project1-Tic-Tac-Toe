b15e42c8d1b513c4b582580d9027a27c943023a7
Azzam Alkhaldi <azzamalkhaldi74@gmail.com>
2019-07-03 12:54:01 +0300

    added a dumb ai

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let turn = false;
 //state of the game
 let ended = false;
 
-
+let ai = false;
 
 
 let xIcon = window.localStorage.getItem('x'),
@@ -73,11 +73,11 @@ const checkWinner = function() {
 const tic = function() {
     let id = this.id;
 
-    const idArr = id.split(',');
+    const idArr = id.split('-');
 
-    const ind1 = parseInt(idArr[0], 10);
+    const ind1 = parseInt(idArr[1], 10);
 
-    const ind2 = parseInt(idArr[1], 10);
+    const ind2 = parseInt(idArr[2], 10);
 
 
     if (turn == false) {
@@ -85,8 +85,8 @@ const tic = function() {
 
         board[ind1][ind2] = 'X';
 
-    } else {
 
+    } else {
         this.innerHTML = `<img src= "${oIcon}" class="game">`;
 
         board[ind1][ind2] = 'O';
@@ -94,11 +94,11 @@ const tic = function() {
     hit.play();
     checkWinner();
     totalScore++;
-    turn = !turn;
     this.removeEventListener('click', tic);
 
+    turn = !turn;
     if (totalScore == 9 && winner == '') {
-        alert('its a draw');
+        document.querySelector('#winner').innerText = 'Its a DRAW!';
         ended = true;
 
     }
@@ -108,6 +108,13 @@ const tic = function() {
         block.forEach(element => {
             element.removeEventListener('click', tic);
         });
+
+
+    if (ai == true) {
+        scan();
+        randomTic();
+
+    }
 }
 
 
@@ -149,3 +156,169 @@ const makeBoard = function() {
 document.querySelector('#reset').addEventListener('click', makeBoard);
 
 makeBoard();
+
+let scannedBoard = [];
+
+
+//smart ai algorythm, will go back to it later
+
+
+// const yourTurn = function() {
+
+//     let thePlace;
+//     let counter;
+//     let found = false;
+//     // console.log('the place is', thePlace);
+
+//     for (let j = 0; j < 3; j++) {
+//         counter = 0;
+//         thePlace = undefined;
+//         for (let i = 0; i < 3; i++) {
+
+//             if (board[j][i] == '') {
+
+//                 thePlace = document.querySelector(`#a-${j}-${i}`)
+//             } else if (board[j][i] == 'x') {
+//                 counter++;
+//             }
+
+
+
+//             console.log('the place is', thePlace);
+//             console.log('counter', counter);
+//             console.log(board[j][i]);
+
+
+//             if (counter == 2 && thePlace != undefined) {
+//                 thePlace.innerHTML = `<img src= "${oIcon}" class="game">`;
+//                 console.log('the place is', thePlace.innerHTML);
+//                 board[j][i] = 'O';
+
+//                 checkWinner();
+//                 turn = false;
+//                 found = true;
+
+
+
+//             }
+
+
+//         }
+
+
+
+//         if (found == true) {
+//             return;
+//         }
+//     }
+
+//     // for (let j = 0; j < 3; j++) {
+//     //     counter = 0;
+//     //     thePlace;
+//     //     for (let i = 0; i < 3; i++) {
+//     //         board[i][j] == '' ? thePlace = board[i][j] : (board[i][j] == 'x' ? counter++ : 'hi');
+
+//     //         if (counter == 2 && thePlace != undefined) {
+//     //             thePlace.innerHTML = `<img src= "${oIcon}" class="game">`;
+
+//     //             board[i][j] = 'O';
+
+//     //             turn = false;
+//     //             found = true;
+
+
+
+//     //         }
+
+//     //     }
+
+//     //     if (found == true) {
+//     //         return;
+//     //     }
+//     // }
+
+
+
+
+//     scan();
+//     randomTic();
+
+
+
+
+
+// }
+
+
+
+
+const randomTic = function() {
+
+    //shuffle the array for a sense of randomness
+    scannedBoard.sort((a, b) => {
+        return Math.random() - 0.5;
+    })
+
+    // debugger;
+    document.querySelector(`#a-${scannedBoard[0]}`).innerHTML = `<img src= "${oIcon}" class="game">`;
+
+    const idArr = scannedBoard[0].split('-');
+
+    const ind1 = parseInt(idArr[0], 10);
+
+    const ind2 = parseInt(idArr[1], 10);
+
+    board[ind1][ind2] = 'O';
+
+    document.querySelector(`#a-${scannedBoard[0]}`).removeEventListener('click', tic);
+
+    if (!ended)
+        checkWinner();
+
+    turn = false;
+
+    if (ended == true)
+        block.forEach(element => {
+            element.removeEventListener('click', tic);
+        });
+
+
+}
+
+let index = 0;
+
+const scan = function() {
+    scannedBoard = [];
+    for (let i = 0; i < 3; i++) {
+
+
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] == '') {
+                scannedBoard[index] = `${i}-${j}`;
+                index++;
+            }
+        }
+
+    }
+    // debugger;
+    // console.log('bye');
+    return scannedBoard;
+}
+
+
+const highlight = function() {
+
+    ai = !ai;
+
+    if (ai == true)
+
+        this.className += ' AI-activated';
+
+    else
+
+        this.className = 'buttons';
+
+}
+
+
+document.querySelector("#AI").addEventListener('click', highlight);
